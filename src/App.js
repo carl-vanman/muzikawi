8a46dab2a7b321154ede4f5727018dfc3a71f6b3
carl-vanman <70966062+carl-vanman@users.noreply.github.com>
2021-06-27 23:56:18 +0200

    banner card

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import logo_text from './img/logo_text.svg'
 
 function App() {
 	return (
@@ -16,14 +17,33 @@ function App() {
 			<main>
 				<section></section>
 				<section className="banner">
-					<article className="banner-content">
-						<div className="img-radio"></div>
-						<p>
-						Agnihicient qui con et poreped expedig enistio beritis mil inulliciam volum quunt, ipistius ut et voluptatur am essum
-						</p>
-						<button className="learn-more">
-							Learn More
-						</button>
+					<article className="banner-card">
+						<div className="img radio"></div>
+						<div className="content">
+							<p>
+							Agnihicient qui con et poreped expedig enistio beritis mil inulliciam volum quunt, ipistius ut et voluptatur am essum
+							</p>
+							<button className="learn-more">
+								Learn More
+							</button>
+						</div>
+					</article>
+					<article className="banner-card">
+						<div className="img podcast">
+							<img src={logo_text} alt="Muzikawi text logo" width="144" height="auto"></img>
+							<h3>Podcast</h3>
+							<p>
+							Itat assequia voluption rest re rati to et pe a sit, consequi
+							</p>
+						</div>
+						<div className="content">
+							<p>
+							Agnihicient qui con et poreped expedig enistio beritis mil inulliciam volum quunt, ipistius ut et voluptatur am essum
+							</p>
+							<button className="learn-more">
+								Learn More
+							</button>
+						</div>
 					</article>
 				</section>
 			</main>
