7c17198f1d2c45a264ce2f0d92d266ce58bb6037
carl-vanman <70966062+carl-vanman@users.noreply.github.com>
2021-06-27 17:35:19 +0200

    img and icons

--- src/App.js
+++ src/App.js
@@ -9,8 +9,16 @@ function App() {
 			</header>
 			<main>
 				<section></section>
-				<section className="banner-mixcont">
-
+				<section className="banner">
+					<article className="banner-content">
+						<div className="img radio"></div>
+						<p>
+						Agnihicient qui con et poreped expedig enistio beritis mil inulliciam volum quunt, ipistius ut et voluptatur am essum
+						</p>
+						<button>
+							Learn More
+						</button>
+					</article>
 				</section>
 			</main>
 			<footer></footer>
