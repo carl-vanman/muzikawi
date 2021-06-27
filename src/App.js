24836722e3f10c74afc8fb126e9e8133a10c3b33
carl-vanman <70966062+carl-vanman@users.noreply.github.com>
2021-06-27 18:14:33 +0200

    continue on banner

--- src/App.js
+++ src/App.js
@@ -6,20 +6,22 @@ function App() {
 			<nav></nav>
 			<header>
 				<section className="hero">
-					<h1>Taking the finest of Ethiopian music to the world!</h1>
-					<p>Itat assequia voluption rest re rati to et pe a sit, consequi que voloren ihictemque alis ipicaep raturi</p>
-					<button className="learn-more">Learn more</button>
+					<div className="img-hero">
+						<h1>Taking the finest of Ethiopian music to the world!</h1>
+						<p>Itat assequia voluption rest re rati to et pe a sit, consequi que voloren ihictemque alis ipicaep raturi</p>
+						<button className="learn-more">Learn more</button>
+					</div>
 				</section>
 			</header>
 			<main>
 				<section></section>
 				<section className="banner">
 					<article className="banner-content">
-						<div className="img radio"></div>
+						<div className="img-radio"></div>
 						<p>
 						Agnihicient qui con et poreped expedig enistio beritis mil inulliciam volum quunt, ipistius ut et voluptatur am essum
 						</p>
-						<button>
+						<button className="learn-more">
 							Learn More
 						</button>
 					</article>
