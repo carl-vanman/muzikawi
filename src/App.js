afac787c665503f4d881d04f85ae309f84e80442
christoseteoglou <70583561+christoseteoglou@users.noreply.github.com>
2021-06-27 17:33:06 +0200

    small header update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
 		<div className="App">
 			<nav></nav>
 			<header>
-				<section></section>
+				<section className="hero">
+					<h1>Taking the finest of Ethiopian music to the world!</h1>
+					<p>Itat assequia voluption rest re rati to et pe a sit, consequi que voloren ihictemque alis ipicaep raturi</p>
+					<button className="learn-more">Learn more</button>
+				</section>
 			</header>
 			<main>
 				<section></section>
