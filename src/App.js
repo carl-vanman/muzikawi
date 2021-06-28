a131d73842ee8793ef577dcf43f74961cf7390d3
carl-vanman <70966062+carl-vanman@users.noreply.github.com>
2021-06-28 12:23:52 +0200

    started on footer

--- src/App.js
+++ src/App.js
@@ -68,7 +68,55 @@ function App() {
 					</article>
 				</section>
 			</main>
-			<footer />
+			<footer>
+				<nav className="footer-nav">
+					<div className="nav browse">
+						<h4>
+							Browse
+						</h4>
+						<ul>
+							<li><a src="#">Home</a></li>
+							<li><a src="#">Releases</a></li>
+							<li><a src="#">Artists</a></li>
+							<li><a src="#">Albums</a></li>
+						</ul>
+					</div>
+					<div className="nav about">
+						<h4>
+							About
+						</h4>
+						<ul>
+							<li><a src="#">Philosophy</a></li>
+							<li><a src="#">Contact us</a></li>
+							<li><a src="#">Team</a></li>
+						</ul>
+					</div>
+					<div className="nav Resources">
+						<h4>
+							Resourses
+						</h4>
+						<ul>
+							<li><a src="#">News Letter</a></li>
+							<li><a src="#">Lefal & DMCA</a></li>
+							<li><a src="#">Privacy Polic</a>y</li>
+							<li><a src="#">FAQ</a></li>
+						</ul>
+					</div>
+				</nav>
+				<form className="contact-form">
+					<h4>Contact us</h4>
+					<label type="text">
+						<input placeholder="Name"></input>
+					</label>
+					<label type="text">
+						<input placeholder="Email"></input>
+					</label>
+					<label type="text">
+						<input placeholder="Message"></input>
+					</label>
+					<button>Send message</button>
+				</form>
+			</footer>
 		</div>
 	);
 }
