0f86785240cabbea3caef20a509fa7df35af65cf
christoseteoglou <christos.eteoglou@iths.se>
2021-06-30 19:40:56 +0200

    updating footer, navbar, socialmedia sections

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import logo_stroke from '../../img/logo_stroke.svg'
 import icn_global from '../../img/icn_Global.svg'
@@ -6,71 +6,59 @@ import './Navbar.css'
 
 function NavBar() {
 	const [click, setClick] = useState(false);
-	const [button, setButton] = useState(true);
 
 	const handleClick = () => setClick(!click);
 	const closeMobileMenu = () => setClick(false);
 
-	const showButton = () => {
-		if (window.innerWidth <= 960) {
-			setButton(false);
-		} else {
-			setButton(true);
-		}
-	};
-
-	useEffect(() => {
-		showButton();
-	}, []);
-
-	window.addEventListener('resize', showButton)
-
 	return (
-	
-			<nav className="navbar">
-				<div className="navbar-container">
-					<Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-					<img src={logo_stroke} width="144" height="auto" />
-					</Link>
-					<div className="menu-icon" onClick={handleClick}>
-						<i className={click ? 'fas fa-times' : 'fas fa-bars'} />
-					</div>
-					<ul className={click ? 'nav-menu active' : 'nav-menu'}>
-						<li className="nav-item">
-							<Link to="/" className="nav-links" onClick={closeMobileMenu}>
-								Label
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link to="/" className="nav-links" onClick={closeMobileMenu}>
-								Publishing
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link to="/" className="nav-links" onClick={closeMobileMenu}>
-								Studio
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link to="/" className="nav-links" onClick={closeMobileMenu}>
-								Live & Closeup
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link to="/" className="nav-links" onClick={closeMobileMenu}>
-								Podcast
-							</Link>
-						</li>
-						<li className="nav-item">
-							<Link to="/" className="nav-links" onClick={closeMobileMenu}>
-								About
-							</Link>
-						</li>
-					<img src={icn_global} width="144" height="auto" className="global" />
-
-					</ul>
+		<nav className="navbar">
+			<div className="navbar-container">
+				<Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+					<img src={logo_stroke} alt="Company logo" width="144" height="auto" />
+				</Link>
+				<div className="menu-icon" onClick={handleClick}>
+					<i className={click ? 'fas fa-times' : 'fas fa-bars'} />
 				</div>
-			</nav>
+				<ul className={click ? 'nav-menu active' : 'nav-menu'}>
+					<li className="nav-item">
+						<Link to="/" className="nav-links" onClick={closeMobileMenu}>
+							Label
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/" className="nav-links" onClick={closeMobileMenu}>
+							Publishing
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/" className="nav-links" onClick={closeMobileMenu}>
+							Studio
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/" className="nav-links" onClick={closeMobileMenu}>
+							Live & Closeup
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/" className="nav-links" onClick={closeMobileMenu}>
+							Podcast
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/" className="nav-links" onClick={closeMobileMenu}>
+							About
+						</Link>
+					</li>
+					<li className="lang-selection">
+						<img src={icn_global} alt="Change language" width="144" height="auto" className="global" />
+						<Link to="/" className="lang-links">English</Link>
+						<div></div>
+						<Link to="/" className="lang-links">Swedish</Link>
+					</li>
+				</ul>
+			</div>
+		</nav>
 
 	)
 }
--- src/Components/SocialMedia/SocialMedia.jsx
+++ src/Components/SocialMedia/SocialMedia.jsx
@@ -13,13 +13,13 @@ const SocialMedia = () => {
 			<img className="logo" src={logo_solid} alt="Muzikawi logo" width="164" height="auto" />
 			<h5>Follow us on our platforms</h5>
 			<div className="platforms">
-				<a src="#">
+				<a href="https://www.facebook.com/">
 					<img src={icon_facebook} alt="Facebook" width="40" height="auto" />
 				</a>
-				<a src="#">
+				<a href="https://www.instagram.com/">
 					<img src={icon_instagram} alt="Instagram" width="40" height="auto" />
 				</a>
-				<a src="#">
+				<a href="https://www.youtube.com/">
 					<img src={icon_youtube} alt="Youtube" width="40" height="auto" />
 				</a>
 			</div>
